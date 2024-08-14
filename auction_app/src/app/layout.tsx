f2c924e11a3d4955a457b52e46980654cabe3e25
Agam Jain <jainagam4@gmail.com>
2024-08-14 20:40:11 +0530

    Add .env and .env.local files and env.ts helper to manage environment variables

--- auction_app/src/app/layout.tsx
+++ auction_app/src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google"
+// import { Inter  } from "next/font/google"
 import "./globals.css";
+import { cn } from "@/lib/utils"
+// const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +22,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    // <html lang="en">
+    //   <body className={inter.className}>{children}</body>
+    // </html>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
