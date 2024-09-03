4da6b8234110e787c075103343dd03bd8128adab
Agam Jain <jainagam4@gmail.com>
2024-09-03 23:50:28 +0530

    Update imports and add KnockProvider and DatePicker components

--- auction_app/src/auth.ts
+++ auction_app/src/auth.ts
@@ -4,12 +4,38 @@ import Google from "next-auth/providers/google"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { database } from "./db/database"
 import { users, accounts, sessions, verificationTokens } from "./db/Schema"
-export const {  handlers, signIn, signOut, auth } = NextAuth({
-    adapter: DrizzleAdapter(database, {
-      usersTable: users,
-      accountsTable: accounts,
-      sessionsTable: sessions,
-      verificationTokensTable: verificationTokens,
-    }),
-    providers: [Google],
-  })
+import { DefaultSession } from "next-auth"; // Add this import statement
+
+declare module "next-auth" {
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's postal address. */
+      id: string;
+      /**
+       * By default, TypeScript merges new interface properties and overwrites existing ones.
+       * In this case, the default session user properties will be overwritten,
+       * with the new ones defined above. To keep the default session user properties,
+       * you need to add them back into the newly declared interface.
+       */
+    } & DefaultSession["user"];
+  }
+}
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(database, {
+    usersTable: users,
+    accountsTable: accounts as any,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+  }),
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+  providers: [Google],
+});
